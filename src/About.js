4bd931a77ef0c3aceb8c7bafcccaadd4f388c29a
triptotest01 <triptotest01>
2023-04-08 11:23:04 +0900

    라우트 테스트

--- src/About.js
+++ src/About.js
@@ -1,3 +1,7 @@
+
+// 테스트 url주소: http://localhost:3000/about?detail=true&mode=1
+
+
 // import React from "react";
 // import {useLocation} from "react-router-dom";
 
@@ -23,20 +27,38 @@
 
 
 
+
+
+// 테스트 url주소: http://localhost:3000/about?detail=true&mode=1
+
 import React from "react";
 import { useSearchParams} from 'react-router-dom';
 
 const About = ()=>{
 
   const [searchParams, setSearchParams] = useSearchParams();
+
   const detail = searchParams.get('detail');
+  console.log(detail)
+
   const mode =searchParams.get('mode');
+  console.log(mode)
 
  const onToggleDetail =()=>{
   setSearchParams({mode, detail:detail==='true'?false:true });
  }
 
  const onIncreaseMode =()=>{
+ // 테스트 !!!  - 방법 : mode +1 버튼 클릭시 함수코드가 시작!
+
+ console.log(typeof(1)) // number
+ console.log(typeof("1")) // string   
+ console.log(typeof(1+"1")) // string
+ console.log(1+"1") // 11
+ console.log(1+1) // 2
+ console.log(mode); //1
+ console.log(typeof(mode)); // string   
+
   const nextMode = mode===null? 1: parseInt(mode) +1;
   setSearchParams({mode: nextMode, detail});
  }
@@ -47,7 +69,8 @@ const About = ()=>{
     <p> 이 사이트는 리액트 라우터를 실습해 보는 예제 프로젝트 입니다.</p>
     <p>detail: {detail}</p>
     <p>mode: {mode} </p>
-    <button onClick={onToggleDetail}>Toggle detail</button>
+
+    <button onClick={onToggleDetail}>Toggle detail</button>&nbsp;&nbsp;
     <button onClick={onIncreaseMode}>mode +1</button>
     
   </div>
--- src/Profile.js
+++ src/Profile.js
@@ -13,12 +13,19 @@ const data= {
 };
 
 const Profile = () => {
+  
  const params = useParams();
+  console.log(params); // {username: 'gaga'}
+
  const profile= data[params.username];
+  console.log(profile); // {name: 'Laday Gaga', description: 'famouse american singer'}
 
   return(
     <div>
       <h1>User profile</h1>
+
+ 테스트: http://localhost:3000/profiles/abcd 
+ {/* abcd 는 없는 파라미터로 undefined 로 profiles 데이터를 찾아올수없고  false 값을 삼항연산자에서 반환 */}
       {profile?(
             <div>   
               <h2>{profile.name}</h2>
